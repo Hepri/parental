bc0d314955fb2070a3daf8d8c8531e349d7ed9d0
Sergey Shcherbina <trytoheal@gmail.com>
2025-10-14 01:44:20 +0500

    chng

--- internal/session/manager.go
+++ internal/session/manager.go
@@ -345,47 +345,43 @@ func (m *Manager) logInUser(account *config.ChildAccount) error {
 	const LOGON_WITH_PROFILE = 0x00000001
 	const CREATE_NEW_CONSOLE = 0x00000010
 
-	// First, try WTSLogonUser to create a real session
-	var userToken windows.Handle
-	// LOGON32_LOGON_INTERACTIVE = 2, LOGON32_PROVIDER_DEFAULT = 0
-	// WTS_CURRENT_SERVER_HANDLE = 0, Logon type set to 2 (interactive)
-	wtsr, _, wtsErr := procWTSLogonUser.Call(
-		WTS_CURRENT_SERVER_HANDLE,
-		0, // Reserved
-		uintptr(unsafe.Pointer(username)),
-		uintptr(unsafe.Pointer(password)),
-		2, // LOGON32_LOGON_INTERACTIVE
-		0, // LOGON32_PROVIDER_DEFAULT
-		uintptr(unsafe.Pointer(&userToken)),
-	)
-	if wtsr != 0 {
-		// Try to switch console to the new session by running tscon
-		// Find target session id (the one owning the token is not trivial to query here),
-		// so we fallback to starting userinit in that session via CreateProcessAsUser
-		var si windows.StartupInfo
-		si.Cb = uint32(unsafe.Sizeof(si))
-		desktop, _ := windows.UTF16PtrFromString("winsta0\\default")
-		si.Desktop = desktop
-		var pi windows.ProcessInformation
-		app, _ := windows.UTF16PtrFromString("C:\\Windows\\System32\\userinit.exe")
-		cr, _, _ := procCreateProcessAsUser.Call(
-			uintptr(userToken),
-			0,
-			uintptr(unsafe.Pointer(app)),
-			0, 0, 0,
-			0,
-			0,
-			uintptr(unsafe.Pointer(&si)),
-			uintptr(unsafe.Pointer(&pi)),
+	// First, try WTSLogonUser to create a real session, if available
+	if procWTSLogonUser.Find() == nil {
+		var userToken windows.Handle
+		wtsr, _, _ := procWTSLogonUser.Call(
+			WTS_CURRENT_SERVER_HANDLE,
+			0, // Reserved
+			uintptr(unsafe.Pointer(username)),
+			uintptr(unsafe.Pointer(password)),
+			2, // LOGON32_LOGON_INTERACTIVE
+			0, // LOGON32_PROVIDER_DEFAULT
+			uintptr(unsafe.Pointer(&userToken)),
 		)
-		if cr != 0 {
-			windows.CloseHandle(windows.Handle(pi.Process))
-			windows.CloseHandle(windows.Handle(pi.Thread))
-			return nil
+		if wtsr != 0 {
+			// Start userinit in that session via CreateProcessAsUser
+			var si windows.StartupInfo
+			si.Cb = uint32(unsafe.Sizeof(si))
+			desktop, _ := windows.UTF16PtrFromString("winsta0\\default")
+			si.Desktop = desktop
+			var pi windows.ProcessInformation
+			app, _ := windows.UTF16PtrFromString("C:\\Windows\\System32\\userinit.exe")
+			cr, _, _ := procCreateProcessAsUser.Call(
+				uintptr(userToken),
+				0,
+				uintptr(unsafe.Pointer(app)),
+				0, 0, 0,
+				0,
+				0,
+				uintptr(unsafe.Pointer(&si)),
+				uintptr(unsafe.Pointer(&pi)),
+			)
+			if cr != 0 {
+				windows.CloseHandle(windows.Handle(pi.Process))
+				windows.CloseHandle(windows.Handle(pi.Thread))
+				return nil
+			}
+			// If CreateProcessAsUser failed, fall through to other methods
 		}
-		// If CreateProcessAsUser failed, fall through to other methods
-	} else {
-		_ = wtsErr
 	}
 
 	// Try with NULL domain (local account) and proper environment/desktop
